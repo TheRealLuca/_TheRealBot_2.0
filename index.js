76936e00dfc6dc55f0f1fbccbce64442a2433ff5
TheRealLuca <92114792+TheRealLuca@users.noreply.github.com>
2021-11-17 19:43:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
-client.login("ODkzMTQzOTUwNjYyMjAxMzU0.YVXLHw.hzA5pOYFk2J2rOsSjRcfpAmwBuo")
+client.login("process.env.token")
 
 client.on("ready", () => {
     console.log("Bot ONLINE")
