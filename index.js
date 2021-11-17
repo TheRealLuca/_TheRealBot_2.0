221491d6fd0144f3c77e16c539b55f2b72d661c3
TheRealLuca <92114792+TheRealLuca@users.noreply.github.com>
2021-11-17 19:34:07 +0100

    Uploads files

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
+
+client.login("ODkzMTQzOTUwNjYyMjAxMzU0.YVXLHw.6-_MHaDxmRH5b_HQalVfZauqD8Q")
+
+client.on("ready", () => {
+    console.log("Bot ONLINE")
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "!youtube") {
+        message.channel.send("A breve il mio canale yt! <3")
+    }
+
+    if (message.content == "!embed") {
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Titolo embed")
+            .setDescription(`${message.author.username} ha scritto il messaggio`)
+            .setThumbnail("https://www.nonsprecare.it/wp-content/uploads/2017/03/come-parlano-alberi-significato-simbolico-1.jpg")
+
+        message.channel.send({ embeds: [embed] })
+    }
+})
